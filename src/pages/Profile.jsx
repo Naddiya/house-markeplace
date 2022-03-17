fe1b305a352f0806b4db2d40c003f886008ba833
Naddiya <nadiamedkouriz@gmail.com>
2022-03-17 17:35:32 +0100

    feat: create listing page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { getAuth, updateProfile } from 'firebase/auth';
 import { db } from '../firebase.config';
 import { updateDoc, doc } from 'firebase/firestore';
 import { toast } from 'react-toastify';
+
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg';
+import homeIcon from '../assets/svg/homeIcon.svg';
 
 const Profile = () => {
   const auth = getAuth();
@@ -36,7 +39,7 @@ const Profile = () => {
         });
       }
     } catch (error) {
-      toast.error('Could not update profile details')
+      toast.error('Could not update profile details');
     }
   };
 
@@ -88,8 +91,13 @@ const Profile = () => {
             value={email}
             onChange={onChange}
           />
-
         </div>
+        <Link to='/create-listing' className='createListing'>
+          <img src={homeIcon} alt="home" />
+          <p>Sell or rent your home</p>
+          <img src={arrowRight} alt="arrow right" />
+        </Link>
+
       </main>
     </div>
   );
