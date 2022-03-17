e23e1cc98b9c17688e0486935bba9ca2237276a7
Naddiya <nadiamedkouriz@gmail.com>
2022-03-17 13:06:32 +0100

    feat: Explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,7 +1,39 @@
+import { Link } from 'react-router-dom';
+
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg';
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg';
+
 const Explore = () => {
   return (
-    <h1>Explore</h1>
-  )
-}
+    <div className="explore">
+      <header>
+        <p className="pageHeader">
+          Explore
+        </p>
 
-export default Explore
+        <main>
+          {/* Slider */}
+
+          <p className="exploreCategoryHeading">Categories</p>
+
+          <div className="exploreCategories">
+            <Link to='/category/rent'>
+              <img src={rentCategoryImage} alt='rent' className='exploreCategoryImg' />
+              <p className="exploreCategoryName">
+                Places For Rent
+              </p>
+            </Link>
+            <Link to='/category/sell'>
+              <img src={sellCategoryImage} alt='rent' className='exploreCategoryImg' />
+              <p className="exploreCategoryName">
+                Places For Sell
+              </p>
+            </Link>
+          </div>
+        </main>
+      </header>
+    </div>
+  );
+};
+
+export default Explore;
